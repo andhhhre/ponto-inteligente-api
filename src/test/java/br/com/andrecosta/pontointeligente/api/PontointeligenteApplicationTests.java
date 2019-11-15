109db49381028cd8ab87d6a64ba163c6e52e718a
André Costa <andrec@hapvida.com.br>
2019-11-15 12:26:22 -0300

    inicial dos arquivos do projeto

---
+++ src/test/java/br/com/andrecosta/pontointeligente/api/PontointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrecosta.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
