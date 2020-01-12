79233bbfe7a5a57563044515b99dcadfe56b85fd
André Costa <andrec@hapvida.com.br>
2020-01-12 16:50:15 -0300

    Adicionado JpaRepository

---
+++ src/main/java/br/com/andrecosta/pontointeligente/api/repositories/LancamentoRepository.java
@@ -0,0 +1,25 @@
+package br.com.andrecosta.pontointeligente.api.repositories;
+
+import java.util.List;
+
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.andrecosta.pontointeligente.api.entities.Lancamento;
+
+@Transactional(readOnly = true)
+@NamedQueries({
+	@NamedQuery(name = "LancamentoRepository.findByFuncionarioId", 
+			query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId") })
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
+
+	List<Lancamento> findByFuncionarioId(@Param("fincionarioId") Long fincionarioId);
+
+	Page<Lancamento> findByFuncionarioId(@Param("fincionarioId") Long fincionarioId, Pageable pageable);
+}
